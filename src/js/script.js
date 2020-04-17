10befd178549ebd2775887fb8aa017717705bdd8
Dmitri <dmitriidemidov93@gmail.com>
2020-04-17 03:19:58 +0300

    slider

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.courousel__inner').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+  });
