3a2ef4fdae4fe0d551cb5109c6f5bc04c7c71079
Dmitri <dmitriidemidov93@gmail.com>
2020-04-19 00:46:38 +0300

    courusel

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
     $('.courousel__inner').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+            }
+          ]
     });
   });
