ed1ee5210099c87058163d8e23b421a172d21a1a
Dmitri <dmitriidemidov93@gmail.com>
2020-04-20 19:54:46 +0300

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -44,5 +44,25 @@ $(document).ready(function(){
     toggleClass('.catalog-item__back');
     toggleClass('.catalog-item__link');
 
+    //Modal 
+
+    $('[data-modal=consultation]').on('click', function(){
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #order, #thanks').fadeOut('fast');
+    });
+
+ /*    $('.button__catalog-item').on('click', function(){
+      $('.overlay, #order').fadeIn('slow');
+    }); */
+
+    $('.button__catalog-item').each(function(i){
+      $(this).on('click', function(){
+        $('#order .modal__desc').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      })
+    });
+
     
   });
