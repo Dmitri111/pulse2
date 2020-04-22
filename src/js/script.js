6b147770a012fdf9f8aefb6401107ed4c7be8f8a
Dmitri <dmitriidemidov93@gmail.com>
2020-04-22 17:10:31 +0300

    send1

--- src/js/script.js
+++ src/js/script.js
@@ -88,5 +88,24 @@ $(document).ready(function(){
     valideForms('#consultation form');
     valideForms('#order form');
 
+
+/*       $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    }); */
+
     
   });
+
+  
